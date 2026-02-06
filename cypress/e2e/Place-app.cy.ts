6671e4d6768b3a9743024eed1d4a1c5130211b68
ctrlbrunad <bruna,dev42@gmail.com>
2026-02-06 14:06:09 -0400

    Testando App

--- cypress/e2e/Place-app.cy.ts
+++ cypress/e2e/Place-app.cy.ts
@@ -1,21 +1,32 @@
 describe("Teste do Place APP", () => {
-  it("should login and access the main page", () => {
+  it("should Register and access the main page", () => {
     cy.viewport("iphone-x");
     cy.visit("localhost:8081/login");
     // credenciais válidas para o teste
-    cy.get('input[placeholder="Email"]').type("teste@gmail.com");
-    cy.get('input[placeholder="Senha"]').type("123");
+    // cy.get('input[placeholder="Nome Completo"]').type("Jorge Luiz");
+    // cy.get('input[placeholder="Email"]').type("teste@place.com");
+    // cy.get('input[placeholder="Senha"]').type("123456789");
+    // cy.contains("CADASTRAR").click();
+    // cy.wait(1000);
+    // fazer o login com o mesmo usuário para acessar a página principal
+    cy.get('input[placeholder="Email"]').type("teste@place.com");
+    cy.get('input[placeholder="Senha"]').type("123456789");
     cy.contains("ENTRAR").click();
-
+    cy.wait(1000);
     cy.contains("Place").should("be.visible"); // verifica se a página principal foi carregada    cy.url().should("include", "/main");
     cy.contains("Home").should("be.visible");
+    cy.wait(1000);
     cy.contains("Perfil").should("be.visible");
     cy.wait(1000);
     cy.contains("Lugares").click();
     cy.wait(1000);
     cy.contains("Horus").click({ force: true });
     cy.wait(1000);
-
-    cy.get('input[placeholder="Favoritar"]').click();
+    cy.contains("Avaliar").click();
+    cy.wait(1000);
+    cy.contains("Cancelar").click();
+    cy.contains("back").click({ force: true });
+    cy.contains("Placelists").click();
+    cy.wait(1000);
   });
 });
